b667561d50f1321d4607a94b30d8bccfbe702e12
gunner10z-school <24jzweiback@athenian.org>
2021-10-27 14:11:03 -0700

    asdf

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,11 +1,31 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed=breed;
+    }
+
+    // A constructor that assumes that a default student is a ninth-grader
+    public Dog(String a, int b) {
+        super(a,b);
+        this.breed = "poodle";
+    }
+
+    // A default/no-argument constructor
+    public Dog() {
+        super("Jerry", 10);
+        this.breed = "poodle";
+    }
 
     // makeNoise() method
+
+    @Override
+    public void makeNoise() {
+        super.makeNoise();
+    }
 
 
     // toString method
@@ -13,6 +33,14 @@ public class Dog extends Pet {
 
     // Getter
 
+    public String getBreed() {
+        return breed;
+    }
+
 
     // Setter
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
